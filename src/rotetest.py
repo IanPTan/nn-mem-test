cd3f3b7050b91cd4d8337afac4d12a62ec846d18
IanPatrickMTan <ianparicktan@gmail.com>
2024-06-02 20:13:06 -0400

    just some more rwkv testing since it works now

--- src/rotetest.py
+++ src/rotetest.py
@@ -6,19 +6,27 @@ from datasets.rotedataset import RoteDataset
 import rwkv
 
 
+device = pt.device('cuda' if pt.cuda.is_available() else 'cpu')
 
-def test(ds, m, ind):
+def test(ds, m, ind, full=False):
+
   model.reset()
   i, t = ds[ind]
+  i, t = i.to(device), t.to(device)
   p = m(i)[5:]
   o = pt.argmax(p, dim=-1)
-  return o, t, p
 
-
+  if full:
+    return o, t, p
+  else:
+    print(f'Sequence comparison for datapoint {ind}:')
+    for it, io in zip(t, o):
+      mark = '✔' if it == io else 'WRONG'
+      print(f'\t{it}\t{io}\t{mark}')
 
 class Model(pt.nn.Module):
 
-  def __init__(self, in_len, mem_len, lay_len, serial=False):
+  def __init__(self, in_len, out_len, mem_len, lay_len, serial=False):
 
     super(Model, self).__init__()
 
@@ -33,7 +41,7 @@ class Model(pt.nn.Module):
         pt.nn.Linear(in_len, mem_len),
         rwkv.DenseNorm(mem_len, mem_len),
         self.rwkv_blocks,
-        pt.nn.Linear(mem_len, in_len),
+        pt.nn.Linear(mem_len, out_len),
         pt.nn.Softmax(dim=-1),
         )
 
@@ -51,20 +59,20 @@ class Model(pt.nn.Module):
     for block in self.rwkv_blocks:
       block.set_serial(state)
 
-
-epochs = 500
+epochs = 5000
 opt_num = 3
-seq_len = 5
+seq_len = 10
 test_ratio = 0.1
-batch_size = 16
+batch_size = 10000
 hidden_size = 32
 lay_len = 4
 
-model = Model(opt_num + 1, hidden_size, lay_len)
+model = Model(opt_num + 1, opt_num, hidden_size, lay_len).to(device)
 dataset = RoteDataset(opt_num, seq_len)
 
 test_size = int(test_ratio * len(dataset))
 train_size = len(dataset) - test_size
+
 batch_len = train_size // batch_size + bool(train_size % batch_size)
 
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
@@ -77,10 +85,11 @@ optimizer = pt.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
 
 all_losses = pt.zeros(epochs)
 pt.autograd.set_detect_anomaly(True)
-print('Starting training.')
+print(f'Starting training using {device}.')
 for epoch in range(epochs):
   losses = pt.zeros(batch_len)
   for i, (inputs, targets) in enumerate(train_loader):
+    inputs, targets = inputs.to(device), targets.to(device)
     optimizer.zero_grad()
     model.reset()
     outputs = model(inputs)[:, seq_len:].permute(0, 2, 1)
