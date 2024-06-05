3e8a379dfba7a2b01c17106cb7f201777cef15b5
IanTitor <iantitor@protonmail.com>
2024-06-04 23:16:32 -0400

    fixed decay so now it decays memory in 2 seperate channels

--- src/rwkv.py
+++ src/rwkv.py
@@ -32,7 +32,7 @@ class Attention(pt.nn.Module):
       mem_len = in_len
     self.mem_len = mem_len
     
-    self.decay = pt.nn.Parameter(pt.zeros(mem_len))
+    self.decay = pt.nn.Parameter(pt.zeros(2, mem_len))
     self.rkv_w = pt.nn.Parameter(pt.randn(mem_len * 3, in_len * 2))
     self.out_w = pt.nn.Parameter(pt.randn(in_len, mem_len))
 
@@ -54,7 +54,7 @@ class Attention(pt.nn.Module):
     rkv = pt.tensordot(xs, self.rkv_w, dims=((-1,), (-1,)))
     r, k, v = rkv.reshape(*rkv.shape[:-1], 3, self.mem_len).movedim(-2, 0)
 
-    kv = pt.stack((pt.exp(k) * v, pt.exp(k)))
+    kv = pt.stack((pt.exp(k) * v, pt.exp(k))).movedim(0, -2)
     d = pt.exp(-pt.exp(self.decay))
 
     if self.serial:
@@ -67,12 +67,12 @@ class Attention(pt.nn.Module):
     else:
       mem = pt.zeros(kv.shape, device=x.device)
       if self.mem == None:
-        self.mem=pt.zeros(mem[..., 0, :].shape, device=x.device)
-      for i in range(kv.shape[-2]):
-        self.mem = self.mem * d + kv[..., i, :]
-        mem[..., i, :] = self.mem
+        self.mem=pt.zeros(mem[..., 0, :, :].shape, device=x.device)
+      for i in range(kv.shape[-3]):
+        self.mem = self.mem * d + kv[..., i, :, :]
+        mem[..., i, :, :] = self.mem
 
-    wkv = mem[0] / mem[1]
+    wkv = mem[..., 0, :] / mem[..., 1, :]
     rwkv = self.sigmoid(r) * wkv
 
     out = pt.tensordot(rwkv, self.out_w, dims=((-1,), (-1,)))
